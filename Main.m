3bb815c98cf20559d2e332f3ae6b6056ae17fada
pking23 <pking23@gmu.edu>
2024-09-23 08:09:35 -0400

    Main.m: Added uigetfolder, Added pipeline folder, Added read_CWA from openmovement github

---
+++ Main.m
@@ -0,0 +1,10 @@
+%%% MAIN SCRIPT %%%
+% Main program allows you select a subject with a CWA.
+
+%%% Local Imports
+% Add function folder to search path
+addpath(genpath('Functions'))
+
+% Select directory
+folder = uigetdir();
+
