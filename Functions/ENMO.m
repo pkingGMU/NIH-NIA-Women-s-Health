50426e4472b56347cc9d0dae4dbab2b0ed166dc9
pking23 <pking23@gmu.edu>
2025-01-31 13:42:26 -0500

    Changed variable to ENMO_j as ENMO is the name of the function to avoid conflicts

--- Functions/ENMO.m
+++ Functions/ENMO.m
@@ -3,44 +3,43 @@ function [zero_frames] = ENMO(data)
 
 
     frame_end = 30000;
-    
-   
-    ax = data(1:frame_end, 2);
-    ay = data(1:frame_end, 3);
-    az = data(1:frame_end, 4);
+
+    ax = data(1:frame_end, 1);
+    ay = data(1:frame_end, 2);
+    az = data(1:frame_end, 3);
 
     % Calculate the magnitude for each frame
     magnitude = sqrt(ax.^2 + ay.^2 + az.^2);
 
     % Subtract 1g to account for gravity and clip negative values to zero
-    ENMO = max(magnitude - 1, 0);
+    ENMO_j = max(magnitude - 1, 0);
 
-    disp(ENMO(1:10));
-
-    % Assuming you have a corresponding time vector `time` for your data
-    time = linspace(0, length(ENMO) * (1/50), length(ENMO));  % Example, adjust based on your sample rate
-    
     % Plot ENMO over time
     figure;
-    plot(ENMO);
+    plot(ENMO_j);
     xlabel('Frames');
     ylabel('ENMO (g)');
     title('ENMO over Time');
     
     % Ask the user for a start frame
-    
+
     start_frame = input('Please Enter Start of Inactivity: ');
 
     % Ask the user for an end frame
-    
+
     end_frame = input('Please Enter End of Inactivity: ');
+
+    %Debug 
+    % start_frame = 26000;
+    % end_frame = 28000;
 
     %%% Find 'Zero frames' or frames with no activity
     % Indicies
-    zero_indices = find(ENMO(start_frame:end_frame) == 0);
+    zero_indices = find(ENMO_j(start_frame:end_frame) == 0);
+    sub_indicies = zero_indices + start_frame -1;
 
     % Set zeroes where user specified
-    zero_frames = zero_indices;
+    zero_frames = sub_indicies;
     
 
 
