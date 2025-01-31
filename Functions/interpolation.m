3558bce47cdbc76b1e41a79d60bc3ecc19c39261
pking23 <pking23@gmu.edu>
2025-01-31 13:42:53 -0500

    Changed total_desired_time to 100 but this needs to change to a variable

--- Functions/interpolation.m
+++ Functions/interpolation.m
@@ -1,12 +1,13 @@
-function [resampled_frames_interpolate] = interpolation(time, data)
+function [resampled_frames_interpolate] = interpolation(time, data, frame_rate)
 %INTERPOLATION Summary of this function goes here
 %   Detailed explanation goes here
     %% Interpolation
     
-    % Create the original time vector based on the variable sample rate (50.2224 frames/sec)
+    % Create the original time vector based on the variable sample rate 
     time_original = linspace(0, time, length(data));
     
-    total_desired_frames = 50 * time;
+    % Debugging
+    total_desired_frames = 100 * time;
     
     % Define the desired time vector for 50 frames per second 
     time_resampled = linspace(0, time, total_desired_frames); 
