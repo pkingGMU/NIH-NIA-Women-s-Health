3897628b238dd1a21f30ed497bc54a3f718076d1
pking23 <pking23@gmu.edu>
2025-02-04 16:50:14 -0500

    SVM in Main_cali

---
+++ Functions/SVM.m
@@ -0,0 +1,36 @@
+% Calculate SVM-1, with optional HP-filter
+% Assumes tri-axial data with the format: [x y z] or [timestamp x y z] in units of 'g'
+%
+% Example:
+%
+% % Load CWA file re-sampled at 100Hz
+% Fs = 100;
+% data = resampleCWA('CWA-DATA.CWA', Fs);
+% 
+% % BP-Filtered SVM-1
+% svm = SVM(data, Fs, 1);
+%
+% % Convert to 60 second epochs (sum of absolute SVM-1 values)
+% epochSVM = epochs(abs(svm), 60 * Fs);
+%
+function svm = SVM(data, Fs, Fc1, Fc2, FN)
+
+    % Defaults for unspecified arguments
+    if nargin < 2; Fs = 100; end    % Sample data frequency (e.g. 100 Hz)
+    if nargin < 3; Fc1 = 0.5; end   % First Cut-off Frequency (typically 0.5 Hz, could be 0.2 Hz)
+    if nargin < 4; Fc2 = 20; end    % Second Cut-off Frequency (typically 20 Hz, could be 15 Hz)
+    if nargin < 5; FN  = 4; end     % Order (typically 4th order)
+
+    % Calculate SVM-1 on last three columns: sqrt(x^2 + y^2 + z^2) - 1;
+    svm = sqrt(sum(data(:, end-2:end) .^ 2, 2)) - 1;
+
+    % Only filter if valid args
+    if Fs > 0 & Fc1 > 0 & Fc2 > 0 & Fc2 > Fc1 & FN > 0
+        % Create Butterworth filter parameters
+        [B,A] = butter(FN, [Fc1, Fc2] ./ (Fs / 2)); 
+
+        % Calculate filtered SVM
+        svm = filter(B, A, svm);
+    end
+	
+end
--- Testing_Environment.m
+++ Testing_Environment.m
@@ -10,6 +10,23 @@ non_active_frames = subjects.subHope.Tables.Stationary(:,1);
 %active_data = pre_filter_AXES(~non_active_frames,:);
 active_data = pre_filter_AXES(~ismember(1:size(pre_filter_AXES, 1), non_active_frames), :);
 
+%%
+% Parameters
+window_size = 100;  % Size of the moving window
+windowed_signal = zeros(size(active_data));  % Preallocate the centered signal
+
+% Moving window loop
+for i = window_size:length(active_data)  % Start from 'window_size' to avoid index out of bounds
+    % Extract the current window of data
+    window_data = active_data(i - window_size + 1:i);  
+    
+    % Calculate the mean of the window
+    window_mean = mean(window_data);  
+    
+    % Subtract the mean from the original signal in the window to center around 0
+    active_data_win(i) = active_data(i) - window_mean;  
+end
+
 %% Root mean squared
 pure_rms = rms(active_data);
 
@@ -18,12 +35,13 @@ Fs = 100;
 
 [b, a] = butter(4,25/(Fs/2), 'low');
 post_filter_AXES = filtfilt(b,a,pre_filter_AXES);
-post_filter_active = filtfilt(b,a,active_data);
+post_filter_active = filtfilt(b,a,active_data_win);
 
 
 %% Plotting
-frame_start = 360000;
-frame_end = 720000;
+frame_start = 1;
+frame_end = 360000;
+
 
 
 processed_subset = post_filter_AXES(frame_start:frame_end, [1,3]); 
